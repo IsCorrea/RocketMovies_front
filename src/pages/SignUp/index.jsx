d607829776202aebb8921fb422f6f39396b4f55e
IsCorrea <bellacorrea12@hotmail.com>
2023-11-30 10:03:07 +0000

    Added the link between front and backend and the functionality of creating users

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,43 +1,77 @@
-import { FiUser, FiMail, FiLock } from 'react-icons/fi'
+import { api } from '../../services/api';
+import { useState } from 'react';
+//import { useNavigate } from 'react-router-dom';
+
+import { FiUser, FiMail, FiLock } from 'react-icons/fi';
 import { FaArrowLeft } from "react-icons/fa";
 
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
 
-import { Input } from "../../components/Input"
-import { Button } from "../../components/Button"
-
-import { Container, Form, Background } from "./styles"
+import { Container, Form, Background } from "./styles";
 
 
 export function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  //const navigate = useNavigate();
+
+  function handleSignUp() {
+    if (! name || ! email || ! password) {
+      alert("Please, fill all fields")
+    }
+
+    api.post("/users", { name, email, password })
+    .then(() => {
+      alert("User successfully created!");
+      //navigate('/');
+    })
+    .catch(error => {
+      if (error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert("The user was not created.");
+      }
+    })
+  }
+
   return(
     <Container>
       <Form>
         <h1>RocketMovies</h1>
         <p>Aplicação para acompanhar tudo que assistir.</p>
 
-        <h2>Crie sua conta</h2>
+        <h2>Create account</h2>
 
         <Input
-          placeholder="Nome"
+          placeholder="Name"
           type="text"
           icon={ FiUser }
+          onChange={e => setName(e.target.value)}
         />
 
         <Input
           placeholder="E-mail"
           type="email"
           icon={ FiMail }
+          onChange={e => setEmail(e.target.value)}
         />
 
         <Input
-          placeholder="Senha"
+          placeholder="Password"
           type="password"
           icon={ FiLock }
+          onChange={e => setPassword(e.target.value)}
         />
 
-        <Button title="Cadastrar"/>
+        <Button
+          title="SignUp"
+          onClick={handleSignUp}
+        />
 
-        <a href='/'><FaArrowLeft/>Voltar para o login</a>
+        <a href='/'><FaArrowLeft/>Back to Sign In</a>
       </Form>
 
       <Background />
